60fdf0a4d25a1f00b954dfd0771c97a77820f2f2
mattk111 <72532026+mattk111@users.noreply.github.com>
2020-10-21 20:44:40 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var movingrect, fixedrect;
+
+function setup() {
+  createCanvas(800,400);
+  movingrect = createSprite(400, 200, 30, 10);
+  fixedrect = createSprite(400, 200, 30, 10);
+  movingrect.shapeColor="green";
+  fixedrect.shapeColor="green";
+}
+
+function draw() {
+  background(0);    
+  movingrect.x = World.mouseX;
+  movingrect.y = World.mouseY; 
+  if (movingrect.x - fixedrect.x < fixedrect.width/2 + movingrect.width/2 &&
+    fixedrect.x - movingrect.x < fixedrect.width/2 + movingrect.width/2 &&
+    movingrect.y - fixedrect.y < fixedrect.height/2 + movingrect.height/2 &&
+    fixedrect.y - movingrect.y < fixedrect.height/2 + movingrect.height/2){
+    movingrect.shapeColor = "red";
+    fixedrect.shapeColor = "red";
+  }
+  else{
+    movingrect.shapeColor = "green";
+    fixedrect.shapeColor = "green";
+  }
+  drawSprites();
+}
